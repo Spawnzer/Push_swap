3a2fde4f25442e0a6da75e9b686912f3aa9cd23a
Alexandre Dubeau <adubeau@c2r4p5.42quebec.com>
2021-08-24 19:19:52 -0400

    rand algo in copie

--- push_swap copie.c
+++ push_swap copie.c
@@ -6,7 +6,7 @@
 /*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 14:44:07 by adubeau           #+#    #+#             */
-/*   Updated: 2021/08/06 14:11:04 by adubeau          ###   ########.fr       */
+/*   Updated: 2021/08/23 15:02:54 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,7 @@ int	ft_is_digit(char c)
 	return (0);
 }
 
-int	ft_atoi(const char *str)
+int	ft_atoi(char *str)
 {
 	int	i;
 	int	neg;
@@ -150,7 +150,7 @@ void	ft_printArrays(int *strA, int *strB, int argc)
 	}
 }
 
-int	ft_swap(int *str)
+int	ft_swap(int *str, int argc)
 {
 	int i;
 	int t;
@@ -163,8 +163,9 @@ int	ft_swap(int *str)
 		t = str[i];
 		str[i] = str[i + 1];
 		str[i + 1] = t;
+		return (1);
 	}
-	return (1);
+	return (0);
 }
 
 int	ft_push(int *strA, int *strB, int argc)
@@ -175,18 +176,18 @@ int	ft_push(int *strA, int *strB, int argc)
 
 
 	i = 0;
-	j = 0;
-	while(strB[j] == NULL)
-		j++;
-	while(strA[i] == NULL)
+	j = argc - 1;
+	while(strB[j] != 0)
+		j--;
+	while(strA[i] == 0)
 		i++;
 	/*ft_putnbr(i);
 	write(1," ", 1);
 	ft_putnbr(j);
 	write(1, "\n", 1);*/
 	t = strA[i];
-	strA[i] = strB[j - 1];
-	strB[j - 1] = t;
+	strA[i] = strB[j];
+	strB[j] = t;
 	return (1);
 }
 
@@ -197,7 +198,7 @@ int	ft_rotate(int *str, int argc)
 	int j;
 
 	i = 0;
-	while(str[i] == (int)NULL)
+	while(str[i] == 0)
 		i++;
 	j = str[i];
 	while(i < argc)
@@ -219,7 +220,8 @@ int	ft_revRotate(int *str, int argc)
 
 	i = argc;
 	j = str[argc];
-	while(i >= 0)
+
+	while(str[i] != 0 && i >= 0)
 	{
 		t = str[i];
 		str[i] = str[i + 1];
@@ -238,7 +240,7 @@ int ft_smallest(int *str, int c, int argc)
 	int k;
 
 	i = c;
-	j = i;
+	j = c;
 	while (i < argc - 1)
 	{
 		if (str[i] < str[j])
@@ -246,6 +248,22 @@ int ft_smallest(int *str, int c, int argc)
 		i++;
 	}
 	return (j - c);
+}
+
+int ft_biggest(int *str, int argc)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i < argc)
+	{
+		if(str[i] > str[j])
+			j = i;
+		i++;
+	}
+	return (str[j]);
 }
 
 int	ft_bubble(int *strA, int *strB, int argc)
@@ -259,44 +277,54 @@ int	ft_bubble(int *strA, int *strB, int argc)
 	i = 0;
 	c = 0;
 	h = 0;
-	ft_printArrays(strA, strB, (argc - 1));
-	while(c < argc - 2)
+	//ft_printArrays(strA, strB, (argc - 1));
+	//write(1, "\n", 1);
+	while(c < argc - 1)
 	{	
 		j = ft_smallest(strA, c, argc);
-		write(1, "j = ", 4);
+	/*	write(1, "j = ", 4);
 		ft_putnbr(j);
-		write(1, "\n", 1);
+		write(1, " Value: ", 8);
+		ft_putnbr(strA[j + c]);
+		write(1, "\n", 1); */
 		k = j;	
 		if(k < ((argc - c) / 2))
 		{		
 			while (k > 0)
 			{
-				h += ft_rotate(strA, argc - 2);
-				write(1, "ra\n", 3);
-				ft_printArrays(strA, strB, (argc - 1));
+				h += ft_rotate(strA, argc);
+			//	write(1, "ra\n", 3);
+			//	ft_printArrays(strA, strB, (argc - 1));
+			//	write(1, "\n", 1);
 				k--;
 			}
 		}
 		else	
 		{
-			while (k + c < argc - 1)
+			while (k + c < argc)
 			{
-				h += ft_revRotate(strA, argc - 2);
-				write(1, "rra\n", 4);
-				ft_printArrays(strA, strB, (argc - 1));
+				h += ft_revRotate(strA, argc);
+			//	write(1, "rra\n", 4);
+			//	ft_printArrays(strA, strB, (argc - 1));
+			//	write(1, "\n", 1);
 				k++;
 			}
 		}
-		h += ft_push(strA, strB, argc - 1);
+		h += ft_push(strA, strB, argc);
 		c++;
-		write(1, "pa\n", 3);
-		ft_printArrays(strA, strB, (argc - 1));
-	}
-	while (strB[argc - 2] != (int)NULL)
+		//write(1, "pa\n", 3);
+	//	ft_printArrays(strA, strB, (argc - 1));
+	//	write(1, "\n", 1);
+			}
+	if (ft_swap(strA, argc))
+		h++;
+		//write(1, "sa\n", 3);
+	while (strB[argc - 1] != 0)
 	{
-		h += ft_push(strB, strA, argc - 1);
-		write(1, "pb\n", 3);
-		ft_printArrays(strA, strB, (argc - 1));
+		h += ft_push(strB, strA, argc);
+	//	write(1, "pb\n", 3);
+	//	ft_printArrays(strA, strB, (argc - 1));
+	//	write(1, "\n", 1);
 	}
 	return (h);
 }
@@ -312,12 +340,12 @@ int	ft_sort3(int *strA)
 	{
 		ret += ft_revRotate(strA, 2);
 		write(1, "rra\n", 4);
-		ret += ft_swap(strA);
+		ret += ft_swap(strA, ret);
 		write(1, "sa\n", 3);
 	}
 	else if (strA[0] > strA[1] && strA[2] > strA[1] && strA[2] > strA[0])
 	{
-		ret += ft_swap(strA);
+		ret += ft_swap(strA, ret);
 		write(1, "sa\n", 3);
 	}
 	else if (strA[0] < strA[1] && strA[1] > strA[2])
@@ -329,7 +357,7 @@ int	ft_sort3(int *strA)
 	{
 		ret += ft_rotate(strA, 2);
 		write(1, "ra\n", 4);
-		ret += ft_swap(strA);
+		ret += ft_swap(strA, ret);
 		write(1, "sa\n", 3);
 	}
 	else
@@ -339,12 +367,205 @@ int	ft_sort3(int *strA)
 	}
 	return (ret);
 }
+int ft_is_sorted(int *strA, int argc)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		j = 0;
+		while (j + i < argc - 1)
+		{
+			if (strA[i] > strA[i + j])
+				return (0);	
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_bogo(int *strA, int *strB, int argc)
+{
+	int i;
+	int (*f[3])(int *, int);
+	int h;
+
+	i = 0;
+	h = 0;
+	f[0] = &ft_swap;
+	f[1] = &ft_revRotate;
+	f[2] = &ft_rotate;
+	while (!ft_is_sorted(strA, argc))
+	{
+		i = rand() % 3;
+		f[i](strA, argc - 2);
+		if (i == 0)
+			write(1, "sa\n", 3);
+		else if (i == 1)
+			write(1, "rra\n", 4);
+		else
+			write(1, "ra\n", 3);
+		ft_printArrays(strA, strB, (argc - 1));
+		h++;
+	}
+	return (h);
+}
+
+int ft_quick(int *strA, int *strB, int argc)
+{
+	int med;
+	int i;
+	int h;
+	int p;
+
+	i = 0;
+	h = 0;
+	p = 0;
+	med = 0;
+	
+	med = (strA[ft_smallest(strA, 0, argc)] + ft_biggest(strA, argc)) / 2;
+	write(1, "Med: ", 5);
+	ft_putnbr(med);
+	write(1, "\n", 1);
+	i = 0;
+	while (i < (argc))
+	{
+		if (strA[p] > med)	
+		{
+			h += ft_push(strA, strB, argc);
+			p++;
+		}
+		else
+			h += ft_rotate(strA, argc - 1);
+		i++;
+		ft_printArrays(strA, strB, (argc));
+		write(1, "\n", 1);
+	}
+	return (h);
+}
+
+int	ft_find(int *str1, int nb)
+{
+	int i;
+
+	i = 0;
+	while (str1[i])
+	{
+		if (str1[i] == nb)
+			return (i + 1);
+		i++;
+	}
+	return (i + 1);
+}
+
+void	ft_switch(int *x, int *y)
+{
+	int t;
+	t = *x;
+	*x = *y;
+	*y = t;
+}
+void ft_quickS(int *strA,  int *strC, int argc)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (strA[i])
+	{
+		j = 0;
+		while (strC[j]) 
+		{
+			if (strA[i] == strC[j])
+			{
+				strA[i] = j + 1;
+			}
+			j++;
+
+		}
+		i++;
+	}
+}
+
+unsigned int ft_binary(unsigned int nb)
+{
+	int rem;
+	int bin;
+	int u;
+
+	u = 1;
+	rem = 0;
+	bin = 0;
+	while (nb != 0)
+	{
+		rem = nb % 2;
+		bin += rem * u;
+		nb = nb / 2;
+		u *= 10;
+	}
+	return (bin);
+}
+
+int	ft_findN(int *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] == 0)
+		i++;
+	return (i);
+}
+
+int ft_radix(int *strA, int *strB, int argc)
+{
+	int max;
+	int max_b;
+	int i;
+	int j;
+	int u;
+	int h;
+
+	max = argc - 1;
+	max_b = 0;
+	i = 1;
+	j = 0;
+	u = 2;
+	h = 0;
+	while (max >> max_b != 0)
+		++max_b;
+	while (i < max_b)
+	{
+		while (j < argc - 1)
+		{
+			u = ft_findN(strA);
+			if (((strA[u] >> i)&1) != 1)
+			{
+				write(1, "\npa", 3);
+				h += ft_push(strA, strB, argc - 1);
+			}
+			 j++;	
+		}
+		j = 0;
+		while (strB[argc - 2] != 0)
+		{
+			write(1, "\npb", 3);
+			h += ft_push(strB, strA, argc - 1);
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+	return (h);
+}
 
 int	main(int argc, char **argv)
 {
 	int i;
-	int strA[argc-1];
+	int strA[argc - 1];
 	int strB[argc - 1];
+	int strC[argc - 1];
 	int ret;
 
 	ret = 0;
@@ -355,23 +576,38 @@ int	main(int argc, char **argv)
 	while (i < argc - 1)
 	{
 		strA[i] = ft_atoi(argv[i + 1]);
+		strC[i] = ft_atoi(argv[i + 1]);
 		strB[i] = 0;
 		i++;		
 	}
+	i = 0;
+	ft_bubble(strC, strB,  argc - 1); // a regler
+	ft_printArrays(strC, strB, argc - 1);
+	write(1, "\n", 1);
+	ft_quickS(strA, strC, argc);
+	/*while (i < argc - 1)
+	{
+		strA[i] = ft_binary(strA[i]);
+		i++;
+	}*/
+	ft_printArrays(strA, strB, argc - 1);
+	ret = ft_radix(strA, strB, argc - 1);
 		/*ft_push(strA, strB, argc - 1);
-		//ft_swap(strA);
+		//ft_swap(strA, argc);
 		ft_revRotate(strA, (argc - 2));
-		ft_swap(strA);
+		ft_swap(strA, argc);
 		ft_rotate(strA, (argc - 2));
 		ft_printArrays(strA, strB, (argc - 1));
 		//write(1, "\n\n", 3);
 		*/
-		if (argc - 1 == 3)
-			ret = ft_sort3(strA);
+	//	if (argc - 1 == 3)
+	//		ret = ft_sort3(strA);
 		//else if (argc - 1 == 5)
 		//	ret = ft_sort5(strA, strB);
-		else
-			ret = ft_bubble(strA, strB, argc);
+		//else
+		//ret = ft_bubble(strA, strB, argc);
+	//	ret = ft_bogo(strA, strB, argc);
+	//	ret = ft_quick(strA, strB, argc - 1);
 		write(1,"Final:\n", 7);
 		ft_printArrays(strA, strB, (argc - 1));
 		write(1, "Moves : ", 8);
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: adubeau <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 14:44:07 by adubeau           #+#    #+#             */
-/*   Updated: 2021/08/06 20:13:38 by adubeau          ###   ########.fr       */
+/*   Updated: 2021/08/24 19:18:34 by adubeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_list
+int	ft_strlen(int str)
 {
-	char			*content;
-	struct s_list	*next;
-}					t_list;
+	int	i;
 
+	i = 0;
+	if (str == 0)
+		i++;
+	while (str != 0)
+	{
+		str = str / 10;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_is_space(char c)
+{
+	if (c == '\f' || c == ' ' || c == '\n' || c == '\r' || c == '\t'
+		|| c == '\v')
+		return (1);
+	return (0);
+}
+
+int	ft_is_sign(char c)
+{
+	if (c == '+' || c == '-')
+		return (1);
+	return (0);
+}
+
+int	ft_is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	neg;
+	int	res;
+
+	i = 0;
+	res = 0;
+	neg = 1;
+	while (ft_is_space(str[i]))
+		i++;
+	if (ft_is_sign(str[i]))
+	{
+		if (str[i] == '-')
+			neg = -1;
+		i++;
+	}
+	while (ft_is_digit(str[i]))
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * neg);
+}
 
 void	ft_putchar(char c)
 {
@@ -29,7 +84,7 @@ void	ft_putchar(char c)
 
 void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 1;
 	while (*str)
@@ -38,113 +93,345 @@ void	ft_putstr(char *str)
 	}
 }
 
-int	ft_strcmp(char *str1, char *str2)
+int	ft_putnbr(int nbr)
 {
-	size_t	i;
+	int			len;
+	int			i;
+	char		deci[25];
+	long		nb;
 
 	i = 0;
-	while (str1[i] == str2[i] && str1[i] && str2[i])
-		i++;
-	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-}
-
-t_list	*ft_addLink(t_list *list, char *str)
-{
-	t_list *tmp;
-
-	tmp = malloc(sizeof(t_list));
-	if (tmp)
+	len = 0;
+	nb = nbr;
+	if (nb == 0)
+		ft_putchar('0');
+	if (nb < 0)
 	{
-		tmp->content = str;
-		tmp->next = list;
+		ft_putchar('-');
+		nb = -nb;
 	}
-	return (tmp);
+	while (nb > 0)
+	{
+		deci[i] = nb % 10 + 48;
+		nb = nb / 10;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+		ft_putchar(deci[i--]);
+	return (len);
 }
-int	ft_lstsize(t_list *lst)
+
+void	ft_printArrays(int *strA, int *strB, int len)
 {
-	unsigned int	i;
+	int	i;
+	int	j;
+	int	space;
 
 	i = 0;
-	while (lst)
+	j = 0;
+	while (i < len)
 	{
-		lst = lst->next;
-		i++;
-	}
-	return (i);
-}
-
-t_list	*ft_lstlast(t_list *lst)
-{
-	while (lst)
-	{
-		if (!lst->next)
-			return (lst);
-		lst = lst->next;
-	}
-	return (lst);
-}
-
-
-
-void	ft_lstadd_back(t_list **alst, t_list *new)
-{
-	t_list	*last;
-
-	if (alst)
-	{
-		if (*alst)
+		ft_putnbr(strA[i]);
+		j = 20 - ft_strlen(strA[i]);
+		while (j)
 		{
-			last = ft_lstlast(*alst);
-			last->next = new;
+			write(1, " ", 1);
+			j--;
 		}
-		else
-			*alst = new;
+		ft_putnbr(strB[i]);
+		write(1, "\n", 1);
+		i++;
 	}
 }
 
-int ft_rotate(t_list *a)
+int	ft_swap(int *str, int argc)
 {
-	char *str;
+	int	i;
+	int	t;
 
-	while (a->next->content)
+	i = 0;
+	while (!str[i])
+		i++;
+	if (str[i] && str[i + 1] && str[i] > str[i + 1])
 	{
-		a->content = a->next->content;
-		ft_putstr(a->content);
-		ft_putchar('\n');
-
-	}	
-}
-
-int	main(int argc,char **argv)
-{
-	t_list *a;
-	t_list *b;
-	int i;
-	
-	i = argc;
-	a = NULL;
-	b = NULL;
-
-	while (i)
-	{
-		a = ft_addLink(a, argv[i]);
-		//a = a->next;
-		i--;
+		t = str[i];
+		str[i] = str[i + 1];
+		str[i + 1] = t;
+		return (1);
 	}
-	while (i++ < argc - 1)
-	{
-		ft_putstr(a->content);
-		ft_putchar('\n');
-		a = a->next;
-	}
-	ft_rotate(a);
-/*	while (i)
-	{
-		ft_putstr(a->content);
-		ft_putchar('\n');
-		a = a->next;
-		i--;
-	}*/
 	return (0);
-	
+}
+
+int	ft_push(int *strA, int *strB, int argc, char c)
+{
+	int	i;
+	int	j;
+	int	t;
+
+	i = 0;
+	j = argc - 1;
+	while (strB[j] != 0)
+		j--;
+	while (strA[i] == 0)
+		i++;
+	t = strA[i];
+	strA[i] = strB[j];
+	strB[j] = t;
+	write(1, "\np", 2);
+	ft_putchar(c);
+	return (1);
+}
+
+int	ft_rotate(int *str, int argc)
+{
+	int	i;
+	int	t;
+	int	j;
+
+	i = 0;
+	while (str[i] == 0)
+		i++;
+	j = str[i];
+	while (i < argc)
+	{
+		t = str[i];
+		str[i] = str[i + 1];
+		str[i + 1] = t;
+		i++;
+	}
+	str[i] = j;
+	write(1, "\nra", 3);
+	return (1);
+}
+
+int	ft_revRotate(int *str, int argc)
+{
+	int	i;
+	int	j;
+	int	t;
+
+	i = argc;
+	j = str[argc];
+	while (str[i] != 0 && i >= 0)
+	{
+		t = str[i];
+		str[i] = str[i + 1];
+		str[i + 1] = t;
+		i--;
+	}
+	i++;
+	str[i] = j;
+	write(1, "\nrra", 4);
+	return (1);
+}
+
+void	ft_switch(int *x, int *y)
+{
+	int	t;
+
+	t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void	ft_sort(int *strA, int len)
+{
+	int	i;
+	int	j;
+	int	t;
+
+	i = 0;
+	while (i < len - 1)
+	{
+		j = 0;
+		while (j < len - i - 1)
+		{
+			if (strA[j] > strA[j + 1])
+				ft_switch(&strA[j], &strA[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	ft_smallest(int *str, int c, int len)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = c;
+	j = c;
+	while (i < len)
+	{
+		if (str[i] < str[j])
+			j = i;
+		i++;
+	}
+	return (j - c);
+}
+
+void	ft_index(int *strA, int *strT, int len)
+{
+	int	i;
+	int	j;
+	int	index;
+
+	i = 0;
+	index = 1;
+	while (i < len)
+	{
+		j = 0;
+		while (j < len)
+		{
+			if (strA[i] > strA[j])
+				index++;
+			j++;
+		}
+		strT[i] = index;
+		index = 1;
+		i++;
+	}
+	i = 0;
+	while (i < len)
+	{
+		strA[i] = strT[i];
+		i++;
+	}
+}
+
+int	ft_findN(int *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == 0)
+		i++;
+	return (str[i]);
+}
+
+int	ft_maxB(int len)
+{
+	int	maxB;
+	int	max;
+
+	max = len;
+	maxB = 0;
+	while (max >> maxB != 0)
+		++maxB;
+	return (maxB);
+}
+
+int	ft_sort3(int *strA)
+{
+	int	ret;
+
+	ret = 0;
+	if (ft_swap(strA, ret))
+	{
+		write(1, "sa\n", 3);
+		ret++;
+	}
+	if (strA[0] > strA[2])
+	{
+		ret += ft_revRotate(strA, 2);
+		write(1, "rra\n", 4);
+	}
+	else if (strA[0] < strA[1] && strA[1] > strA[2])
+	{
+		ret += ft_revRotate(strA, 2);
+		write(1, "rra\n", 4);
+	}
+	if (ft_swap(strA, ret))
+	{
+		write(1, "sa\n", 3);
+		ret++;
+	}
+	return (ret);
+}
+
+int	ft_bubble(int *strA, int *strB, int len)
+{
+	int	j;
+	int	c;
+	int	h;
+
+	c = 0;
+	h = 0;
+	while (c < len - 2)
+	{
+		j = ft_smallest(strA, c, len);
+		if (j < ((len - c) / 2))
+			while (j-- > 0)
+				h += ft_rotate(strA, len - 1);
+		else
+			while (j++ + c < len)
+				h += ft_revRotate(strA, len - 1);
+		h += ft_push(strA, strB, len, 'a');
+		c++;
+	}
+	if (ft_swap(strA, len))
+		h++;
+	while (strB[len - 1] != 0)
+		h += ft_push(strB, strA, len, 'b');
+	return (h);
+}
+
+int	ft_radix(int *strA, int *strB, int len)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	max;
+	int	u;
+
+	i = -1;
+	j = 0;
+	u = 0;
+	max = ft_maxB(len);
+	while (++i < max)
+	{
+		while (j++ < len)
+		{
+			k = ft_findN(strA);
+			if (((k >> i) & 1) == 1)
+				 u += ft_rotate(strA, len - 1);
+			else
+				u += ft_push(strA, strB, len, 'a');
+		}
+		j = 0;
+		while (strB[len - 1] != 0)
+			u += ft_push(strB, strA, len, 'b');
+	}
+	return (u);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	len;
+	int	strA[argc - 1];
+	int	strB[argc - 1];
+	int	strT[argc - 1];
+
+	i = -1;
+	len = argc - 1;
+	while (++i < len)
+	{
+		strA[i] = ft_atoi(argv[i + 1]);
+		strB[i] = 0;
+		strT[i] = strA[i];
+	}
+	ft_index(strA, strT, len);
+	i = 0;
+	if (len == 3)
+		i += ft_sort3(strA);
+	else if (len == 5)
+		i += ft_bubble(strA, strB, len);
+	else
+		i = ft_radix(strA, strB, len);
+	ft_printArrays(strA, strB, len);
+	write(1, "\nFinal: ", 7);
+	ft_putnbr(i);
 }
